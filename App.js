72fc087f94575ae41b25dc2f26c74434a87b6e96
wira sudharma <wira.sudharma24@gmail.com>
2020-12-12 01:33:41 +0700

    next commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Image, View} from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source = {{uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png'}}
+   style = {{ width: 200, height: 200 }}
+   />
+      <InputUsername id="txtUsername" name="txtUsername"/>
+      <InputPassword/>
+      <BtnSubmit/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +19,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const InputUsername = (props) =>  <Input placeholder="Comment" leftIcon={{ type: 'font-awesome-5', name: 'user' }} />
+const InputPassword = (props) => <Input placeholder="Password" leftIcon={{type: 'font-awesome-5', name: 'unlock-alt'}} secureTextEntry={true} />
+const BtnSubmit = (props) => <Button icon={{ name: "arrow-right",size: 15,color: "white"}} title="Button with icon object"
+/>
